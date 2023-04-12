48a2065bc02b4d762ea54d1030721873f4089935
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-04-12 09:25:39 +0300

    sc

--- guessNumberApp/App.jsx
+++ guessNumberApp/App.jsx
@@ -4,10 +4,13 @@ import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 import {LinearGradient} from "expo-linear-gradient";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Colors from "./constants/colors";
 import {useFonts} from "expo-font";
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
@@ -23,8 +26,18 @@ export default function App() {
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   })
 
+  useEffect(() => {
+    console.log('fontsLoaded', fontsLoaded)
+    // Hide the splash screen after fetching resources
+    if (fontsLoaded) {
+      setTimeout(() => {
+        SplashScreen.hideAsync();
+      }, 10000);  
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return;
   }
 
   let screen = <StartGameScreen onPickedNumber={pickedNumberHandler} />;
