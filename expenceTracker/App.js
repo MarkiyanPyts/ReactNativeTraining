f9254d55a8cc8cbb624fd4886b7722b70a7fb573
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-04-24 09:06:49 +0300

    work

--- expenceTracker/App.js
+++ expenceTracker/App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ManageExpence from './screens/ManageExpence';
+import RecentExpences from './screens/RecentExpances';
+import AllExpances from './screens/AllExpances';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpancesOverview() {
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen name="RecentExpences" component={RecentExpences} />
+    <BottomTabs.Screen name="All Expences" component={AllExpances} />
+  </BottomTabs.Navigator>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpancesOverview" component={ExpancesOverview} />
+          <Stack.Screen name="ManageExpence" component={ManageExpence} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
