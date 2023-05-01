3428bd843c7e94ff1fb32e8e56437045f9aa9f07
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-05-01 08:37:52 +0300

    Update ExpenceForm.js

--- expenceTracker/components/ManageExpence/ExpenceForm.js
+++ expenceTracker/components/ManageExpence/ExpenceForm.js
@@ -5,26 +5,38 @@ import Button from '../UI/Button';
 import { getFormattedDate } from '../../util/date';
 
 function ExpenceForm({submitButtonLabel, onCancel, onSubmit, defaultValues}) {
-    const [inputValues, setInputValues] = useState({
-        amount: defaultValues ? defaultValues.amount.toString() : '',
-        date: defaultValues ? getFormattedDate(defaultValues.date) : '',
-        description: defaultValues ? defaultValues.description : '',
+    const [inputs, setInputs] = useState({
+        amount: {
+            value: defaultValues ? defaultValues.amount.toString() : '',
+            isValid: true,
+        },
+        date: {
+            value: defaultValues ? getFormattedDate(defaultValues.date) : '',
+            isValid: true,
+        },
+        description: {
+            value: defaultValues ? defaultValues.description : '',
+            isValid: true,
+        },
     });
 
     function inputChangeHandler(inputIdentifier, enteredValue) {
-        setInputValues((currentInputValues) => {
+        setInputs((currInputs) => {
             return {
-                ...currentInputValues,
-                [inputIdentifier]: enteredValue,
+                ...currInputs,
+                [inputIdentifier]: {
+                    value: enteredValue,
+                    isValid: true
+                },
             }
         })
     }
 
     function submitHandler () {
         const expenceData = {
-            amount: +inputValues.amount,
-            date: new Date(inputValues.date),
-            description: inputValues.description,
+            amount: +inputs.amount.value,
+            date: new Date(inputs.date.value),
+            description: inputs.description.value,
         }
 
         const amountIsVsalid = !isNaN(expenceData.amount) && expenceData.amount > 0;
@@ -32,12 +44,30 @@ function ExpenceForm({submitButtonLabel, onCancel, onSubmit, defaultValues}) {
         const descriptionIsValid = expenceData.description.trim().length > 0;
 
         if (!amountIsVsalid || !dateIsValid || !descriptionIsValid) {
-            Alert.alert('Invalid input', 'Please check your input values', [{text: 'OK'}]);
+            //Alert.alert('Invalid input', 'Please check your input values', [{text: 'OK'}]);
+            setInputs((currInputs) => {
+                return {
+                    amount: {
+                        value: currInputs.amount.value,
+                        isValid: amountIsVsalid,
+                    },
+                    date: {
+                        value: currInputs.date.value,
+                        isValid: dateIsValid,
+                    },
+                    description: {
+                        value: currInputs.description.value,
+                        isValid: descriptionIsValid,
+                    }
+                }
+            });
             return;
         }
 
         onSubmit(expenceData);
     }
+
+    const formIsInvalid = !inputs.amount.isValid || !inputs.date.isValid || !inputs.description.isValid;
 
     return <View style={styles.form}>
         <Text style={styles.title}>Your Expence</Text>
@@ -50,7 +80,7 @@ function ExpenceForm({submitButtonLabel, onCancel, onSubmit, defaultValues}) {
                     onChangeText: (enteredValue) => {
                         inputChangeHandler('amount', enteredValue);
                     },
-                    value: inputValues.amount,
+                    value: inputs.amount.value,
                 }}
             />
             <Input
@@ -62,7 +92,7 @@ function ExpenceForm({submitButtonLabel, onCancel, onSubmit, defaultValues}) {
                     onChangeText: (enteredValue) => {
                         inputChangeHandler('date', enteredValue);
                     },
-                    value: inputValues.date,
+                    value: inputs.date.value,
                 }}
             />            
         </View>
@@ -74,9 +104,10 @@ function ExpenceForm({submitButtonLabel, onCancel, onSubmit, defaultValues}) {
                 onChangeText: (enteredValue) => {
                     inputChangeHandler('description', enteredValue);
                 },
-                value: inputValues.description,
+                value: inputs.description.value,
             }} />
         </View>
+        {formIsInvalid && <Text>Invalid Input Values - please check your entered data!</Text>}
         <View style={styles.buttons}>
             <Button style={styles.button} mode={'flat'} onPress={onCancel}>Cancel</Button>
             <Button style={styles.button} onPress={submitHandler}>{submitButtonLabel}</Button>
