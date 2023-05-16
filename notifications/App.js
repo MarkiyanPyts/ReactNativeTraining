23324341ffc3351f8e4d5a3948dcd897912ef298
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-05-16 09:07:44 +0300

    note

--- notifications/App.js
+++ notifications/App.js
@@ -1,9 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+const allowsNotificationsAsync = async () => {
+  const settings = await Notifications.getPermissionsAsync();
+  return (
+    settings.granted ||
+    settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  );
+};
+ 
+const requestPermissionsAsync = async () => {
+  return await Notifications.requestPermissionsAsync({
+    ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+  });
+};
 
 export default function App() {
+  async function scheduleNotificationHandler() {
+    console.log("scheduleNotification");
+    const hasPushNotificationPermissionGranted = await allowsNotificationsAsync();
+
+
+    if (!hasPushNotificationPermissionGranted) {
+      await requestPermissionsAsync();
+    }
+
+    const notification = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Look at that notification',
+        body: "I'm so proud of myself!",
+        data: { userName: 'Mark' }
+      },
+      trigger: null,
+    });
+
+    console.log(notification);
+  }
   return (
     <View style={styles.container}>
+      <Button title="Schedule Notification" onPress={scheduleNotificationHandler} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
