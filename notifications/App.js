f1aaf7e435aed85590619f3362f076db0e54bdf6
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-05-17 07:46:01 +0300

    Update App.js

--- notifications/App.js
+++ notifications/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Button } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -31,6 +32,23 @@ const requestPermissionsAsync = async () => {
 };
 
 export default function App() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
+      console.log("notification received:", notification);
+      const userName = notification.request.content.data.userName;
+      console.log("userName:", userName);
+    });
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log("response:", response);
+      console.log("responseContent:", response.notification.request.content.data.userName);
+    });
+
+    return () => {
+      subscription1.remove()
+      subscription2.remove()
+    };
+  }, []);
   async function scheduleNotificationHandler() {
     console.log("scheduleNotification");
     const hasPushNotificationPermissionGranted = await allowsNotificationsAsync();
